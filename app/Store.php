60fb6a169c3e8400b947dabc047ee34571dd580d
sjuliper7 <sjuliper7@gmail.com>
2018-10-29 17:19:45 +0700

    remove redundant attribute "store_address"

--- app/Store.php
+++ app/Store.php
@@ -8,9 +8,9 @@ class Store extends Model
 {
     protected $fillable = [
         'store_name', 'store_owner', 'store_email', 'store_phone',
-        'store_address','store_ktp','store_ktp_image','store_address',
-        'store_npwp','store_npwp_image','store_account_bank',
-        'store_account_type','store_account_bank_image','id_status',
+        'store_address','store_ktp','store_ktp_image', 'store_npwp',
+        'store_npwp_image','store_account_bank', 'store_account_type',
+        'store_account_bank_image','id_status',
         'id_user'
     ];
 
