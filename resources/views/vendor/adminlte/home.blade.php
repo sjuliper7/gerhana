ca310c732f3e4cb28fab4e914bdce4668d6aaa4d
sjuliper7 <sjuliper7@gmail.com>
2018-10-29 17:24:51 +0700

    add menu for access page store request

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -39,6 +39,21 @@
                     </div>
                 </div>
 
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3>{{count($storeRequest)}}</h3>
+
+                            <p>Store Request</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-paper-plane" style="font-size:72px" ></i>
+                        </div>
+                        <a href="/store-request" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
             </div>
         </section>
     </div>
