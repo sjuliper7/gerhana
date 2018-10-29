23024ff1889b57a820c9a69d4e13ab163c685cfb
sjuliper7 <sjuliper7@gmail.com>
2018-10-29 17:23:06 +0700

    add new page

---
+++ resources/views/store/request-store.blade.php
@@ -0,0 +1,365 @@
+@extends('layouts.index-for-detail')
+
+@section('header')
+    <nav class="main_nav">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <div class="main_nav_content d-flex flex-row">
+
+                        <!-- Categories Menu -->
+
+                        <div class="cat_menu_container">
+                            <div class="cat_menu_title d-flex flex-row align-items-center justify-content-start">
+                                <div class="cat_burger"><span></span><span></span><span></span></div>
+                                <div class="cat_menu_text">Kategori</div>
+                            </div>
+
+                            <ul class="cat_menu">
+                                <li><a href="#">Pakaian <i class="fas fa-chevron-right ml-auto"></i></a></li>
+                                <li><a href="#">Cenderamata<i class="fas fa-chevron-right"></i></a></li>
+                                <li><a href="#">Ukiran<i class="fas fa-chevron-right"></i></a></li>
+                                <!--
+                                <li class="hassubs">
+                                    <a href="#">Ukiran<i class="fas fa-chevron-right"></i></a>
+                                    <ul>
+                                        <li class="hassubs">
+                                            <a href="#">Menu Item<i class="fas fa-chevron-right"></i></a>
+                                            <ul>
+                                                <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                                <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                                <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                                <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                        <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                        <li><a href="#">Menu Item<i class="fas fa-chevron-right"></i></a></li>
+                                    </ul>
+                                </li>
+                            -->
+                                <li><a href="#">Patung<i class="fas fa-chevron-right"></i></a></li>
+                                <li><a href="#">Buku<i class="fas fa-chevron-right"></i></a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Main Nav Menu -->
+
+                        <div class="main_nav_menu ml-auto">
+                            <ul class="standard_dropdown main_nav_dropdown">
+                                <li class="page_menu_item">
+                                    <a href="/">Home<i class="fa fa-angle-down"></i></a>
+                                </li>
+
+                                <li class="page_menu_item"><a href="blog.html">blog<i class="fa fa-angle-down"></i></a></li>
+                                <li class="page_menu_item"><a href="contact.html">contact<i class="fa fa-angle-down"></i></a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Menu Trigger -->
+
+                        <div class="menu_trigger_container ml-auto">
+                            <div class="menu_trigger d-flex flex-row align-items-center justify-content-end">
+                                <div class="menu_burger">
+                                    <div class="menu_trigger_text">menu</div>
+                                    <div class="cat_burger menu_burger_inner"><span></span><span></span><span></span></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- Menu -->
+
+    <div class="page_menu">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <div class="page_menu_content">
+
+                        <div class="page_menu_search">
+                            <form action="#">
+                                <input type="search" required="required" class="page_menu_search_input" placeholder="Cari produk...">
+                            </form>
+                        </div>
+                        <ul class="page_menu_nav">
+                            <li class="page_menu_item has-children">
+                                <a href="#">Language<i class="fa fa-angle-down"></i></a>
+                                <ul class="page_menu_selection">
+                                    <li><a href="#">English<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">Italian<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">Spanish<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">Japanese<i class="fa fa-angle-down"></i></a></li>
+                                </ul>
+                            </li>
+                            <li class="page_menu_item has-children">
+                                <a href="#">Currency<i class="fa fa-angle-down"></i></a>
+                                <ul class="page_menu_selection">
+                                    <li><a href="#">US Dollar<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">EUR Euro<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">GBP British Pound<i class="fa fa-angle-down"></i></a></li>
+                                    <li><a href="#">JPY Japanese Yen<i class="fa fa-angle-down"></i></a></li>
+                                </ul>
+                            </li>
+                            <li class="page_menu_item">
+                                <a href="#">Home<i class="fa fa-angle-down"></i></a>
+                            </li>
+
+                            <li class="page_menu_item"><a href="blog.html">blog<i class="fa fa-angle-down"></i></a></li>
+                            <li class="page_menu_item"><a href="contact.html">contact<i class="fa fa-angle-down"></i></a></li>
+                        </ul>
+
+                        <div class="menu_contact">
+                            <div class="menu_contact_item"><div class="menu_contact_icon"><img src="template/images/phone_white.png" alt=""></div>+38 068 005 3570</div>
+                            <div class="menu_contact_item"><div class="menu_contact_icon"><img src="template/images/mail_white.png" alt=""></div><a href="mailto:fastsales@gmail.com">batakzone@gmail.com</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('content')
+    <style>
+        .tab{
+            display: none;
+        }
+
+        /* Step marker: Place in the form. */
+        .step {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbbbbb;
+            border: none;
+            border-radius: 50%;
+            display: inline-block;
+            opacity: 0.5;
+        }
+
+        .step.active {
+            opacity: 1;
+        }
+
+        /* Mark the steps that are finished and valid: */
+        .step.finish {
+            background-color: #4CAF50;
+        }
+    </style>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-10 mx-auto" >
+                <center><h3>Request Toko</h3></center><br>
+                <form action="/stores" method="post" id="myForm" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <div class="tab">
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label>Nama Toko</label>
+                                <input type="text" class="form-control" id="storeName" placeholder="Nama Toko" name="store-name">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Nama Pemilik</label>
+                                <input type="text" class="form-control" id="storeOwner" placeholder="Nama Pemilik" name="store-owner">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label>Email</label>
+                                <input type="text" class="form-control" id="storeName" placeholder="Email" name="store-email">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Nomor Telepon</label>
+                                <input type="text" class="form-control" id="storeOwner" placeholder="Nomor Telepon" name="store-phone">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-12">
+                                <label>Alamat</label>
+                                <textarea class="form-control" name="store-address"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label>Nomor KTP</label>
+                                <input type="text" class="form-control" id="idKtp" placeholder="Nomor KTP" name="store-ktp">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Foto KTP</label>
+                                <div class="form-group">
+                                    <img src="http://placehold.it/400x400" id="show_ktp" style="max-width:200px;max-height:200px;" class="center-block" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="btn btn-info center-block">
+                                        Browse
+                                        <input type="file" id="input_ktp" name="ktp-image" style="display: none">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label>Nomor NPWP</label>
+                                <input type="text" class="form-control" id="idKtp" placeholder="Nomor NPWP" name="store-npwp">
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Foto NPWP</label>
+                                <div class="form-group">
+                                    <img src="http://placehold.it/400x400" id="show_npwp" style="max-width:200px;max-height:200px;" class="center-block" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="btn btn-info center-block">
+                                        Browse
+                                        <input type="file" id="input_npwp" name="npwp-image" style="display: none">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <div class="form-group row">
+                            <div class="col-sm-6">
+                                <label>Nomor Rekening</label>
+                                <input type="text" class="form-control" id="idKtp" placeholder="Nomor Rekening" name="store-account-number">
+                                <br>
+                                <label>Jenis Bank</label>
+                                <select class="custom-select custom-select-lg mb-3" name="type-bank">
+                                    <option selected>Pilih Bank</option>
+                                    <option value="1">BRI</option>
+                                    <option value="2">BNI</option>
+                                    <option value="3">MANDIRI</option>
+                                </select>
+                            </div>
+                            <div class="col-sm-6">
+                                <label>Foto Rekening</label>
+                                <div class="form-group">
+                                    <img src="http://placehold.it/400x400" id="show_account" style="max-width:200px;max-height:200px;" class="center-block" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="btn btn-info center-block">
+                                        Browse
+                                        <input type="file" id="input_account" name="account-image" style="display: none">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div style="float:right;">
+                            <button type="button"  id="prevBtn" class="btn btn-default" onclick="nextPrev(-1)" style="margin-left: 20px">Previous</button>
+                            <button type="button"  id="nextBtn" class="btn btn-success" onclick="nextPrev(1)">Next</button>
+                            <button type="submit"  id="submit" class="btn btn-success" style="display: none">Submit</button>
+                        </div>
+                    </div>
+                    <!-- Circles which indicates the steps of the form: -->
+                    <div style="text-align:center;margin-top:40px;">
+                        <span class="step"></span>
+                        <span class="step"></span>
+                        <span class="step"></span>
+                        <span class="step"></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- alerts are for fun of it -->
+    <div class="alerts-container">
+
+        <div class="row">
+            <div id="click-alert" class="alert alert-warning" role="alert">
+            </div>
+        </div>
+
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#input_ktp").change(function () {
+                var ktp = '#show_ktp';
+                readURL(this, ktp);
+            });
+            $("#input_npwp").change(function () {
+                var npwp = '#show_npwp';
+                readURL(this, npwp)
+            })
+            $("#input_account").change(function () {
+                var account = '#show_account';
+                readURL(this, account)
+            })
+        })
+
+        function readURL(input, target) {
+
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    $(target).attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        var currentTab = 0; // Current tab is set to be the first tab (0)
+        showTab(currentTab); // Display the crurrent tab
+
+        function showTab(n) {
+            var x = document.getElementsByClassName("tab");
+            x[n].style.display = "block";
+            //... and fix the Previous/Next buttons:
+            if (n == 0) {
+                document.getElementById("prevBtn").style.display = "none";
+            } else {
+                document.getElementById("prevBtn").style.display = "inline";
+            }
+            if (n == (x.length - 1)) {
+                document.getElementById("nextBtn").hidden = true;
+                $("#submit").show();
+            } else {
+                document.getElementById("nextBtn").innerHTML = "Next";
+            }
+
+            fixStepIndicator(n)
+        }
+
+        function nextPrev(n) {
+            var x = document.getElementsByClassName("tab");
+            x[currentTab].style.display = "none";
+            currentTab = currentTab + n
+            if (currentTab >= x.length) {
+                document.getElementById("regForm").submit();
+                return false;
+            }
+            showTab(currentTab);
+        }
+
+
+        function fixStepIndicator(n) {
+            var i, x = document.getElementsByClassName("step");
+            for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace(" active", "");
+            }
+            x[n].className += " active";
+        }
+
+    </script>
+
+@endsection
---
+++ resources/views/vendor/adminlte/stores/request-store.blade.php
@@ -0,0 +1,43 @@
+@extends('adminlte::layouts.app')
+
+@section('title', '| Users')
+
+@section('main-content')
+
+    <div class="col-lg-10 col-lg-offset-1">
+        <h1><i class="fa fa-paper-plane"></i>Store Request</h1>
+        <hr>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Store Name</th>
+                    <th>Store Email</th>
+                    <th>Owner</th>
+                    <th>Created at</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                @foreach ($stores as $store)
+                    <tr>
+
+                        <td>{{ $store->store_name }}</td>
+                        <td>{{ $store->store_name }}</td>
+                        <td>{{ $store->store_owner }}</td>
+                        <td>{{ $store->created_at->format('F d, Y h:ia') }}</td>{{-- Retrieve array of roles associated to a user and convert to string --}}
+
+                        <td>
+                            <a href="{{ route('stores.show', $user->id) }}" class="btn btn-info pull-left" style="margin-right: 3px;">Detail</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+
+            </table>
+        </div>
+
+    </div>
+
+@endsection
