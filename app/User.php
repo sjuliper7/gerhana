fead2b8550ab23ec0f099d3e84439ad8067c6075
sjuliper7 <sjuliper7@gmail.com>
2018-10-30 17:30:09 +0700

    add function requestStore

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Store','id_user');
     }
+
+    public function requestStore()
+    {
+        return $this->hasOne('App\RequestStore','id_user');
+    }
 }
