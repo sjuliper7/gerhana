961be1f8aa88437e57b93b78adf01e7326559604
sjuliper7 <sjuliper7@gmail.com>
2018-10-30 17:29:29 +0700

    change several attribute, remove function status, add function request store

--- app/Store.php
+++ app/Store.php
@@ -10,16 +10,15 @@ class Store extends Model
         'store_name', 'store_owner', 'store_email', 'store_phone',
         'store_address','store_ktp','store_ktp_image', 'store_npwp',
         'store_npwp_image','store_account_bank', 'store_account_type',
-        'store_account_bank_image','id_status',
+        'store_account_bank_image','id_request',
         'id_user'
     ];
 
-    public function status(){
-        return $this->belongsTo('App\StatusStore','id_status');
+    public function requestStore(){
+        return $this->belongsTo('App\RequestStore','id_request');
     }
 
     public function user(){
         return $this->belongsTo('App\User','id_user');
     }
-
 }
