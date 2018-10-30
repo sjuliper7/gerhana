37fb832837bf37d510b56bb4b9415019c923a3a0
sjuliper7 <sjuliper7@gmail.com>
2018-10-30 17:16:24 +0700

    complete function store

--- app/Http/Controllers/StoreRequestController.php
+++ app/Http/Controllers/StoreRequestController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\RequestStore;
 use App\StatusStore;
 use App\Store;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StoreRequestController extends Controller
 {
@@ -15,8 +17,8 @@ class StoreRequestController extends Controller
      */
     public function index()
     {
-        $status = StatusStore::where('name','PANDING')->firstOrFail();
-        $stores = Store::where('id_status',$status->id )->orderby('id', 'desc')->get();
+        $status = StatusStore::where('name','PENDING')->firstOrFail();
+        $stores = RequestStore::where('id_status',$status->id )->orderby('id', 'desc')->get();
         return view('adminlte::store-requests.index',compact('stores'));
     }
 
@@ -27,7 +29,7 @@ class StoreRequestController extends Controller
      */
     public function create()
     {
-        //
+        return view('store.request-store');
     }
 
     /**
@@ -38,7 +40,41 @@ class StoreRequestController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $status = StatusStore::where('name','PENDING')->firstOrFail();
+
+        //instance object for StatusStore
+        $requestStore = new RequestStore();
+        $requestStore->store_name         = $request['store-name'];
+        $requestStore->store_owner        = $request['store-owner'];
+        $requestStore->store_email        = $request['store-email'];
+        $requestStore->store_phone        = $request['store-phone'];
+        $requestStore->store_address      = $request['store-address'];
+        $requestStore->store_ktp          = $request['store-ktp'];
+        $requestStore->store_npwp         = $request['store-npwp'];
+        $requestStore->store_account_bank = $request['store-account-number'];
+        $requestStore->store_account_type = $request['type-bank'];
+        $requestStore->id_status          = $status->id;
+        $requestStore->id_user            = Auth::user()->id;
+
+        $ktpFile    = $request->file('ktp-image');
+        $ktpFileName   = $ktpFile->getClientOriginalName();
+        $request->file('ktp-image')->move('images/',$ktpFileName);
+
+        $npwpFile    = $request->file('npwp-image');
+        $npwpFileName   = $npwpFile->getClientOriginalName();
+        $request->file('npwp-image')->move('images/',$npwpFileName);
+
+        $accountFile    = $request->file('account-image');
+        $accountFileName   = $accountFile->getClientOriginalName();
+        $request->file('account-image')->move('images/',$accountFileName);
+
+        $requestStore->store_ktp_image             = $ktpFileName;
+        $requestStore->store_npwp_image            = $npwpFileName;
+        $requestStore->store_account_bank_image    = $accountFileName;
+
+        $requestStore->save();
+
+        return redirect('my-store');
     }
 
     /**
@@ -49,7 +85,8 @@ class StoreRequestController extends Controller
      */
     public function show($id)
     {
-        //
+        $store = Store::findOrFail($id);
+        return view("adminlte::store-requests.show",compact('store'));
     }
 
     /**
