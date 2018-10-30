4f1e2e6d649f731edc08cd21e6eeace3d8e6d725
sjuliper7 <sjuliper7@gmail.com>
2018-10-30 17:34:04 +0700

    comment menu products

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -24,33 +24,33 @@
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-xs-6">
-                    <!-- small box -->
-                    <div class="small-box bg-aqua">
-                        <div class="inner">
-                            <h3>{{count($products)}}</h3>
+                {{--<div class="col-lg-3 col-xs-6">--}}
+                    {{--<!-- small box -->--}}
+                    {{--<div class="small-box bg-aqua">--}}
+                        {{--<div class="inner">--}}
+                            {{--<h3>{{count($products)}}</h3>--}}
 
-                            <p>Products</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-shopping-bag" style="font-size:72px" ></i>
-                        </div>
-                        <a href="/products" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
+                            {{--<p>Products</p>--}}
+                        {{--</div>--}}
+                        {{--<div class="icon">--}}
+                            {{--<i class="fa fa-shopping-bag" style="font-size:72px" ></i>--}}
+                        {{--</div>--}}
+                        {{--<a href="/products" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
 
                 <div class="col-lg-3 col-xs-6">
                     <!-- small box -->
                     <div class="small-box bg-green">
                         <div class="inner">
-                            <h3>{{count($storeRequest)}}</h3>
+                            <h3>{{count($requestStores)}}</h3>
 
-                            <p>Store Request</p>
+                            <p>Request Store</p>
                         </div>
                         <div class="icon">
                             <i class="fa fa-paper-plane" style="font-size:72px" ></i>
                         </div>
-                        <a href="/store-requests" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="/request-stores" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
 
