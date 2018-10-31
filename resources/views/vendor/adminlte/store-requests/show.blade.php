68917871afd3e06f9d972576a06774f331317ae8
sjuliper7 <sjuliper7@gmail.com>
2018-10-31 14:38:59 +0700

    completing page detail for request store

--- resources/views/vendor/adminlte/store-requests/show.blade.php
+++ resources/views/vendor/adminlte/store-requests/show.blade.php
@@ -8,101 +8,155 @@
         <div class="box-header with-border">
             <h3 class="box-title"> DETAIL STORE REQUEST</h3>
         </div>
-        <div class="box-body">
-            <table class="table table-user-information">
+        <div class="box-body ">
+            <table class="table table-user-information col-lg-12">
                 <tr>
                     <th>Store Name</th>
                     <td>: </td>
-                    <td>{{$store->store_name}}</td>
+                    <td>{{$requestStore->store_name}}</td>
                 </tr>
                 <tr>
                     <th>Owner</th>
                     <td>: </td>
-                    <td>{{$store->store_owner}}</td>
+                    <td>{{$requestStore->store_owner}}</td>
                 </tr>
                 <tr>
                     <th>Email</th>
                     <td>: </td>
-                    <td>{{$store->store_email}}</td>
+                    <td>{{$requestStore->store_email}}</td>
                 </tr>
                 <tr>
                     <th>Phone</th>
                     <td>: </td>
-                    <td>{{$store->store_phone}}</td>
+                    <td>{{$requestStore->store_phone}}</td>
                 </tr>
                 <tr>
                     <th>Address</th>
                     <td>: </td>
-                    <td>{{$store->store_address}}</td>
+                    <td>{{$requestStore->store_address}}</td>
                 </tr>
                 <tr>
                     <th>No KTP</th>
                     <td>: </td>
-                    <td>{{$store->store_ktp}}</td>
+                    <td>{{$requestStore->store_ktp}}</td>
                 </tr>
                 <tr>
                     <th>KTP Image</th>
                     <td>: </td>
                     <td>
-                        <a href="/images/{{$store->store_ktp_image}}" data-lightbox="roadtrip">
-                            <img src="/images/{{$store->store_ktp_image}}" style="max-height:200px;max-width: 200px">
+                        <a href="/images/{{$requestStore->store_ktp_image}}" data-lightbox="roadtrip">
+                            <img src="/images/{{$requestStore->store_ktp_image}}" style="max-height:200px;max-width: 200px">
                         </a>
                     </td>
                 </tr>
                 <tr>
                 <th>No NPWP</th>
                     <td>: </td>
-                    <td>{{$store->store_npwp}}</td>
+                    <td>{{$requestStore->store_npwp}}</td>
                 </tr>
                 <tr>
                     <th>NPWP Image</th>
                     <td>: </td>
                     <td>
-                        <a href="/images/{{$store->store_npwp_image}}" data-lightbox="roadtrip">
-                            <img src="/images/{{$store->store_npwp_image}}" style="max-height:200px;max-width: 200px">
+                        <a href="/images/{{$requestStore->store_npwp_image}}" data-lightbox="roadtrip">
+                            <img src="/images/{{$requestStore->store_npwp_image}}" style="max-height:200px;max-width: 200px">
                         </a>
                     </td>
                 </tr>
                 <tr>
                     <th>Account Number</th>
                     <td>: </td>
-                    <td>{{$store->store_account_bank}}</td>
+                    <td>{{$requestStore->store_account_bank}}</td>
                 </tr>
                 <tr>
                     <th>Type Of Bank</th>
                     <td>: </td>
-                    <td>{{$store->store_account_type}}</td>
+                    <td>{{$requestStore->store_account_type}}</td>
                 </tr>
                 <tr>
                     <th>NPWP Image</th>
                     <td>: </td>
                     <td>
-                        <a href="/images/{{$store->store_npwp_image}}" data-lightbox="roadtrip">
-                            <img src="/images/{{$store->store_npwp_image}}" style="max-height:200px;max-width: 200px">
+                        <a href="/images/{{$requestStore->store_account_bank_image}}" data-lightbox="roadtrip">
+                            <img src="/images/{{$requestStore->store_account_bank_image}}" style="max-height:200px;max-width: 200px">
                         </a>
                     </td>
                 </tr>
-                <tr>
-                    <th></th>
-                    <td>: </td>
-                    <td>
-                    <td>
-                        <form action="{{url('/'.$store->id)}}" method="post">
-                            {{csrf_field()}}
-                            <input type="hidden" name="_method" value="PUT">
-                            <button type="submit" class="btn btn-info"><i class="fa fa-check"> Terima</i></button>
 
+                <tr>
+                    <th>Sent Comment</th>
+                    <td>: </td>
+                    <td>  <form class="" action="index.html" method="post">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-newspaper-o"></i>
+                                    </div>
+                                    <textarea class="form-control" placeholder="Comment"  id="comment" style="height:200px;"
+                                              name="comment">{{$requestStore->comment}}</textarea>
+                                </div>
+                                <span id="errfn2" style="color: red"></span>
+                            </div>
                         </form>
-                    </td>
+                </tr>
+
+                <td>
+                    <th></th>
                     <td>
-                        <a class="btn btn-danger" onclick="cancelRequest()"> <i class="fa fa-close"></i>Tolak </a>
+                        <div style="float: right;">
+                            <div class="col-sm-6">
+                                <form action="{{url('request-stores/'.$requestStore->id)}}" method="post">
+                                    {{csrf_field()}}
+                                    <input type="hidden" name="_method" value="PUT">
+                                    <input type="hidden" name="value" value="1">
+                                    <button type="submit" class="btn btn-info"><i class="fa fa-check"> Terima</i></button>
+                                </form>
+                            </div>
+                            <div class="col-sm-4">
+                                <a class="btn btn-danger" onclick="cancelRequest()"> <i class="fa fa-close"></i>Tolak</a>
+                            </div>
+                        </div>
                     </td>
                 </tr>
             </table>
         </div>
+
         <div class="box-footer">
 
         </div>
     </div>
+
+    <script>
+        var uri = 'request-stores/'+'{{$requestStore->id}}';
+        function cancelRequest() {
+
+            console.log(url);
+            var check = confirm("Apakah anda yakin untuk menolak nya??");
+            var fiedl = document.getElementById('comment');
+
+            if (fiedl.value == ' ' || fiedl.value == null || fiedl.value == '  '
+                || fiedl.value == '-') {
+                alert('Pesan Harus di isi');
+            } else {
+                if (check) {
+                    $.ajax({
+                        data: {
+                            value: fiedl.value,
+                            requestID: '{{$requestStore->id}}',
+                            _method : 'PUT',
+                            _token: '{{ csrf_token() }}',
+                        },
+                        url: uri,
+                        type: 'POST',
+                        success: function (data) {
+                            console.log("success")
+                        }
+                        // window.location.reload(true);
+
+                    });
+                }
+            }
+        }
+    </script>
 
 @endsection
