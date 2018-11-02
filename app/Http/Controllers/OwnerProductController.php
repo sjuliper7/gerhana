41d363fe7cba1d3a86cd3b68a57e7a95a2a96593
Johan Radot Mangaratua Lumban Batu <johanlbatu@gmail.com>
2018-11-02 13:23:20 +0700

    Make function create

--- app/Http/Controllers/OwnerProductController.php
+++ app/Http/Controllers/OwnerProductController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\CategoryProduct;
+use App\StatusProduct;
 use Illuminate\Http\Request;
 
 class OwnerProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+        return view('owner-product.index');
+    }
+
+    public function create(){
+        $statusProducts = StatusProduct::all();
+        $categoryProducts = CategoryProduct::all();
+        return view('owner-product.create',compact('statusProducts','categoryProducts'));
     }
 }
