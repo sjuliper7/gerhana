62a74dc7f8b8a2775b28e352c34c22897a3d2e1e
sjuliper7 <sjuliper7@gmail.com>
2018-11-04 18:09:18 +0700

    modifying method index and add method store

--- app/Http/Controllers/OwnerProductController.php
+++ app/Http/Controllers/OwnerProductController.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers;
 
 use App\CategoryProduct;
 use App\StatusProduct;
+use App\Store;
 use Illuminate\Http\Request;
 
 class OwnerProductController extends Controller
 {
-    public function index(){
-        return view('owner-product.index');
+    public function index($name){
+        $store = Store::where(['store_name' => $name])->firstOrFail();
+        $products = $store->products;
+
+        return view('owner-product.index',compact('products'));
     }
 
     public function create(){
         $statusProducts = StatusProduct::all();
         $categoryProducts = CategoryProduct::all();
         return view('owner-product.create',compact('statusProducts','categoryProducts'));
+    }
+
+    public function store(Request $request){
+
+        foreach ($request->parameters as $image){
+            echo $image;
+            echo '<br>';
+        }
+
+        dd(count($request->parameters));
     }
 }
