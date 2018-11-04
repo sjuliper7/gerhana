5ac21f551aba24c256ffb64d20b4f515dba95357
sjuliper7 <sjuliper7@gmail.com>
2018-11-04 18:23:06 +0700

    modifying layout

--- resources/views/owner-product/create.blade.php
+++ resources/views/owner-product/create.blade.php
@@ -9,119 +9,139 @@
         <div class="container">
             <h2>Create Product</h2>
             <hr>
-
-            <form action="/products" method="post" id="myForm" data-toggle="validator" role="form" enctype="multipart/form-data">
+            <form action="/owner-products" method="post" id="myForm" data-toggle="validator" role="form" enctype="multipart/form-data">
                 {{ csrf_field() }}
-                <div class="row">
-                <div class="col-sm-4">
-                    <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" name="name" class="form-control" placeholder="Name" data-error="Please enter name" required>
-                        <div class="help-block with-errors"></div>
-                    </div>
+                <div class="form-row">
+                     <div class="col-md-6">
+                         <div class="form-row">
+                             <div class="form-group col-md-12">
+                                 <label>Name</label>
+                                 <input type="text" name="name" class="form-control" placeholder="Name" data-error="Please enter name" required>
+                                 <div class="help-block with-errors"></div>
+                             </div>
+                         </div>
+                         <div class="form-row">
+                             <div class="form-group col-md-6">
+                                 <label>Price</label>
+                                 <input type="text" name="price" class="form-control" placeholder="Price" data-error="Please enter price" required>
+                                 <div class="help-block with-errors"></div>
+                             </div>
+                             <div class="form-group col-md-6">
+                                 <label>Stock</label>
+                                 <input type="text" name="stock" class="form-control" placeholder="stock" data-error="Please enter stock" required>
+                                 <div class="help-block with-errors"></div>
+                             </div>
+                         </div>
+                         <div class="form-row">
+                             <div class="form-group col-md-5">
+                                 <label>Category Product</label>
+                                 <select name="category-select" id="select-category" class="form-control" required style="width: 100%">
+                                     <option selected="selected" name="category-selected">Select Category</option>
+                                     @foreach($categoryProducts as $categoryProduct)
+                                         <option value="{{$categoryProduct->id}}">{{$categoryProduct->name}}</option>
+                                     @endforeach
+                                 </select>
 
-                    <div class="form-group">
-                        <label>Category Product</label>
-                        <select name="category-select" id="select-status" class="form-control" data-live-search="true" data-error="Please select user" required>
-                            <option selected="selected" name="category-selected">Select Category</option>
-                            @foreach($categoryProducts as $categoryProduct)
-                                <option value="{{$categoryProduct->id}}">{{$categoryProduct->name}}</option>
-                            @endforeach
-                        </select>
-                        <div class="help-block with-errors"></div>
-                    </div>
+                             </div>
 
-                    <div class="form-group">
-                        <label>Price</label>
-                        <input type="text" name="price" class="form-control" placeholder="Price" data-error="Please enter price" required>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                    <div class="form-group">
-                        <label>Stock</label>
-                        <input type="text" name="stock" class="form-control" placeholder="stock" data-error="Please enter stock" required>
-                        <div class="help-block with-errors"></div>
-                    </div>
-                    <div class="form-group">
-                        <label>Status Product</label>
-                        <select name="status-select" id="select-status" class="selectpicker form-control" data-live-search="true" data-error="Please select user" required>
-                            <option selected="selected" name="status-selected">Select Status</option>
-                            @foreach($statusProducts as $statusProduct)
-                                <option value="{{$statusProduct->id}}">{{$statusProduct->name}}</option>
-                            @endforeach
-                        </select>
-                        <div class="help-block with-errors"></div>
-                    </div>
+                             <div class="form-group col-md-5">
+                                 <label>Status Product</label>
+                                 <select name="status-select" id="select-status" class="form-control" style="width: auto">
+                                     <option selected="selected" name="status-selected">Select Status</option>
+                                     @foreach($statusProducts as $statusProduct)
+                                         <option value="{{$statusProduct->id}}">{{$statusProduct->name}}</option>
+                                     @endforeach
+                                 </select>
+                             </div>
 
-                    <div class="form-group">
-                        <label>Description</label>
-                        <textarea name="description" class="form-control" rows="4" placeholder="Description" data-error="Please enter description"  required></textarea>
-                        <div class="help-block with-errors"></div>
-                    </div>
+                         </div>
+                         <div class="form-row">
+                             <div class="form-group col-md-12">
+                             <label>Description</label>
+                             <textarea name="description" class="form-control" rows="4" placeholder="Description" data-error="Please enter description"  required></textarea>
+                             <div class="help-block with-errors"></div>
+                             </div>
+                         </div>
 
+                         <div style="float: right;margin-bottom: 20px">
+                             <input type="button" id="cancel" value="Cancel" class="btn btn-danger " style="margin-top: 10px;">
+                             <input type="submit" id="add"value="Create Product" class="btn btn-info" style="margin-top: 10px;">
+                         </div>
 
+                     </div>
 
-
-
-                    <div style="float: right;margin-bottom: 20px">
-                        <input type="button" id="cancel" value="Cancel" class="btn btn-danger " id="add" style="margin-top: 10px;">
-                        <input type="submit" value="Create Product" class="btn btn-info " id="add" style="margin-top: 10px;">
-                    </div>
-
-
-
-                </div>
-                    <div class="col-sm-4"style="margin:5em;">
-
+                    <div class="col-md-6">
                         <div class="form-group">
-                            <img src="http://placehold.it/400x400" id="show_image" style="max-width:300px;max-height:300px;" class="center-block" />
+                            <label>Images</label>
+                            <table class="table" id="dynamic_field">
+                                <tr>
+                                    <td>
+                                        <div class="form-group">
+                                            <img src="http://placehold.it/400x400" id="show_image-" style="max-width:100px;max-height:100px;" class="center-block" />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <label class="btn btn-info">Browse<input type="file" id="input_image-" name="images[]" style="display: none" onchange="loadImage(this)"></label>
+                                    </td>
+                                    <td><button type="button" name="addImages" id="more" class="btn btn-success">Add More</button></td>
+                                </tr>
+                            </table>
                         </div>
-
-                        <div class="form-group">
-                            <label class="btn btn-info center-block" style="margin-top: 5px; margin-left: 30px; margin-right: 30px">
-                                Browse
-                                <input type="file" id="input_image" name="image" style="display: none">
-                            </label>
-                        </div>
-
                     </div>
                 </div>
-
-
-
-
             </form>
-
-
 
         </div>
     </div>
 
     <script type="text/javascript">
 
-        function readURL(input) {
+        $(document).ready(function () {
+            var i=1;
+            $('#more').click(function(){
+                i++;
+                $('#dynamic_field').append('<tr id="row'+i+'">' +
+                    '<td><div class="form-group"><img src="http://placehold.it/400x400" id="show_image-'+i+'" style="max-width:100px;max-height:100px;" class="center-block" /></div></td>'+
+                    '<td><label class="btn btn-info">Browse<input type="file" id="input_image-'+i+'" name="images[]" onchange="loadImage(this)" style="display: none"></label></td>' +
+                    '<td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td>' +
+                    '</tr>');
+            });
+            $(document).on('click', '.btn_remove', function(){
+                var button_id = $(this).attr("id");
+                $('#row'+button_id+'').remove();
+            });
+        });
+
+        function loadImage(input){
+            var data = input.id.split("-");
+            var variable;
+            if(data[1] === ""){
+                variable = "#show_image-";
+            }else{
+                variable = "#show_image-"+ data[1];
+            }
 
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
-
+                console.log(variable);
                 reader.onload = function(e) {
-                    $('#show_image').attr('src', e.target.result);
+                    $(variable).attr('src', e.target.result);
                 }
-
                 reader.readAsDataURL(input.files[0]);
             }
+
         }
 
         $(function () {
             $("#input_image").change(function () {
-                // console.log("test")
-                readURL(this);
+                console.log("test")
+                // readURL(this);
             });
 
             $('#cancel').click(function () {
                 $('#myForm')[0].reset();
             });
 
-            // $('#select-status').selectpicker();
         });
 
 
