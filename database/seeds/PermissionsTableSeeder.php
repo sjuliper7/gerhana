c29b5eedf95dc47261ad4f5ad8db22962ee726f0
sjuliper7 <sjuliper7@gmail.com>
2018-11-06 08:06:05 +0700

    add code to create all table related to permission [RBAC]

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -37,5 +37,50 @@ class PermissionsTableSeeder extends Seeder
             ]
         );
 
+        DB::table('model_has_permissions')->insert(
+            [
+                'role_id' => 1,
+                'model_type' => "App\User",
+                'model_id' => 1,
+            ]
+        );
+
+        DB::table('model_has_permissions')->insert(
+            [
+                'role_id' => 2,
+                'model_type' => "App\User",
+                'model_id' => 2,
+            ]
+        );
+
+        DB::table('model_has_permissions')->insert(
+            [
+                'role_id' => 2,
+                'model_type' => "App\User",
+                'model_id' => 3,
+            ]
+        );
+
+        DB::table('role_has_permissions')->insert(
+            [
+                'permission_id' => 1,
+                'role_id' => 1,
+            ]
+        );
+
+        DB::table('role_has_permissions')->insert(
+            [
+                'permission_id' => 2,
+                'role_id' => 2,
+            ]
+        );
+
+        DB::table('role_has_permissions')->insert(
+            [
+                'permission_id' => 3,
+                'role_id' => 2,
+            ]
+        );
+
     }
 }
