bd2bb5354083c8d246ab2682ada60df485dca1b3
sjuliper7 <sjuliper7@gmail.com>
2018-11-06 13:31:04 +0700

    add variable product

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\CategoryProduct;
+use App\Product;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
     public function index(){
         $categoryProducts = CategoryProduct::all();
-        return view('landing-page',compact('categoryProducts'));
+        $products = Product::all();
+        return view('landing-page',compact('categoryProducts', 'products'));
     }
 }
