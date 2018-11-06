1995f69058c30b4943aa70e0afe0bd53afd4664c
sjuliper7 <sjuliper7@gmail.com>
2018-11-06 08:26:30 +0700

    change table model has permissions to model has role

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -37,7 +37,7 @@ class PermissionsTableSeeder extends Seeder
             ]
         );
 
-        DB::table('model_has_permissions')->insert(
+        DB::table('model_has_roles')->insert(
             [
                 'role_id' => 1,
                 'model_type' => "App\User",
@@ -45,7 +45,7 @@ class PermissionsTableSeeder extends Seeder
             ]
         );
 
-        DB::table('model_has_permissions')->insert(
+        DB::table('model_has_roles')->insert(
             [
                 'role_id' => 2,
                 'model_type' => "App\User",
@@ -53,7 +53,7 @@ class PermissionsTableSeeder extends Seeder
             ]
         );
 
-        DB::table('model_has_permissions')->insert(
+        DB::table('model_has_roles')->insert(
             [
                 'role_id' => 2,
                 'model_type' => "App\User",
