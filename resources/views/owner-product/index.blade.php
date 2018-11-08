e577029248f62e8dc8991b02ad4cc2e91fe36864
Johan Radot Mangaratua Lumban Batu <johanlbatu@gmail.com>
2018-11-08 13:53:53 +0700

    changes layout for owner-product index

--- resources/views/owner-product/index.blade.php
+++ resources/views/owner-product/index.blade.php
@@ -5,7 +5,7 @@
         <div class="home_background parallax-window" data-parallax="scroll" data-image-src="images/shop_background.jpg"></div>
         <div class="home_overlay"></div>
         <div class="home_content d-flex flex-column align-items-center justify-content-center">
-            <h2 class="home_title">My Products</h2>
+            <h2 class="home_title">Toko</h2>
         </div>
     </div>
 
@@ -34,11 +34,30 @@
                 </div>
 
                 <div class="col-lg-9">
+                    <div class="row">
+                    <div class="col-sm-2"style="margin-left:-1em">
+                        <img src="images/shop.png" class="img-thumbnail" alt="...">
+                    </div>
+                    <div class="col-sm-7">
+                        <h3>Toko TB Silalahi</h3>
+                        <h5>johanlbatu@gmail.com</h5>
+                        <h5> Laguboti, Kab. Toba Samosir, Sumatra Utara</h5>
+                    </div>
+                    </div>
 
                     <!-- Shop Content -->
 
+
                     <div class="shop_content">
-                        <a class="btn btn-info" href="{{url('owner-products/create')}}">Create Product</a>
+                        <div class="row"style="margin-top: 1em">
+                            <button type="button" class="btn btn-primary"style="margin: 0.5em">Menu 1</button>
+                            <button type="button" class="btn btn-secondary"style="margin: 0.5em">Menu 2</button>
+                            <button type="button" class="btn btn-success"style="margin: 0.5em">Menu 3</button>
+                            <a href="{{url('owner-products/create')}}">
+                                <button type="button" class="btn btn-primary"style="margin: 0.5em">Create Product</button>
+                                </a>
+                        </div>
+
                         <div class="shop_bar clearfix">
                             <div class="shop_product_count"><span>186</span> products found</div>
                             <div class="shop_sorting">
@@ -59,36 +78,7 @@
                         <div class="product_grid">
                             <div class="product_grid_border"></div>
 
-                            @foreach($products as $product)
-                                <!-- Product Item -->
-                                    <div class="product_item">
-                                        <div class="product_border"></div>
-                                        <div class="product_image d-flex flex-column align-items-center justify-content-center">
-                                            <?php
-                                                $images = json_decode($product->images);
-                                            ?>
-                                            <img src="{{ asset('images/'.$images[0]) }}" alt="" style="width:150px;height:150px; object-fit: cover;" >
-                                        </div>
-                                        <div class="product_content">
-                                            <div class="product_price">Rp {{$product->price}}</div>
-                                            <div class="product_name">
-                                                <div>
-                                                    <a href="product.html" tabindex="0">{{$product->name}}
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="product_fav"><i class="fas fa-heart"></i></div>
-                                        <ul class="product_marks">
-                                            <li class="product_mark product_discount">-25%</li>
-                                            <li class="product_mark product_new">new</li>
-                                        </ul>
-                                        <div class="row">
-                                            <a href="{{url('owner-products/'.$product->id)}}" class="btn btn-info" >Detail</a>
-                                        </div>
-                                    </div>
 
-                                @endforeach
 
 
                         </div>
