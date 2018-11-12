7a3e0582fdf1cdd09f6811c9d8f93aac4d82f655
sjuliper7 <sjuliper7@gmail.com>
2018-11-12 20:18:15 +0700

    add new migration for table cart

---
+++ database/migrations/2018_11_09_082316_create_cart_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->double('sub_total_price');
+            $table->boolean('is_active');
+            $table->integer('id_product')->unsigned();
+            $table->integer('id_user')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
