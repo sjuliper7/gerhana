333b3e99e96a4ebb1744a3e30da8b90cc9eea129
jhon <if415011@students.del.ac.id>
2018-11-13 11:33:46 +0700

    edit user-profile

--- resources/views/vendor/adminlte/user-profile/edit.blade.php
+++ resources/views/vendor/adminlte/user-profile/edit.blade.php
@@ -25,7 +25,7 @@
 
                 <div class="form-group">
                     <label>Tanggal Lahir</label>
-                    <input type="text" name="date_of_birth" class="form-control" value="{{$profile->date_of_birth}}" data-error="Please enter name" required>
+                    <input type="date" name="date_of_birth" class="form-control" value="{{$profile->date_of_birth}}" data-error="Please enter name" required>
                     <div class="help-block with-errors"></div>
                 </div>
 
@@ -37,50 +37,13 @@
 
                 <div class="form-group">
                     <label>Image Profile</label>
-                    <input type="text" name="profile_image" class="form-control" value="{{$profile->profile_image}}" data-error="Please enter Profile Image" required>
+                    <img src="{{ asset('image/'.$profile->profile-image) }}" id="showgambar" style="max-width:200px;max-height:200px;float:left;" />
+                    {{--<input type="text" name="profile_image" class="form-control" value="{{$profile->profile_image}}" data-error="Please enter Profile Image" required>--}}
                     <div class="help-block with-errors"></div>
                 </div>
 
 
                 <input type="submit" value="Edit User Status" class="btn btn-success btn-lg btn-block" id="add" style="margin-top: 70px;">
-            </form>
-
-            <form action="/user-profile" method="post" data-toggle="validator" role="form">
-                {{ csrf_field() }}
-
-                <div class="form-group">
-                    <label>Gambar Profil</label>
-                    <img src="http://placehold.it/100x100" id="show_profile" style="max-width:200px;max-height:200px;" class="center-block" />
-                    <div class="help-block with-errors"></div>
-
-
-                    <label class="btn btn-info center-block">
-                        Browse
-                        <input type="file" id="input_image_profile" name="profile_image" style="display: none">
-                    </label>
-
-                </div>
-
-                <div class="form-group">
-                    <label>Nama Lengkap</label>
-                    <input type="text" name="full_name" class="form-control" placeholder="Nama Lengkap" data-error="Please enter full name" required>
-                    <div class="help-block with-errors"></div>
-                </div>
-
-                <div class="form-group">
-                    <label>Tanggal Lahir</label>
-                    <input type="date" name="date_of_birth" class="form-control" placeholder="Tanggal Lahir" data-error="Please enter born date" required>
-                    <div class="help-block with-errors"></div>
-                </div>
-
-                <div class="form-group">
-                    <label>Alamat</label>
-                    <input type="text" name="address" class="form-control" placeholder="Alamat" data-error="Please enter address" required>
-                    <div class="help-block with-errors"></div>
-                </div>
-
-
-                <input type="submit" value="Create User Profile" class="btn btn-success btn-lg btn-block" id="add" style="margin-top: 70px;">
             </form>
         </div>
     </div>
