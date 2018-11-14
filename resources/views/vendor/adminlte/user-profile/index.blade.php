ca20d9ec93e7c641301d361ad111396d254d7485
jhon <if415011@students.del.ac.id>
2018-11-14 20:27:43 +0700

    completed index for User Profile

--- resources/views/vendor/adminlte/user-profile/index.blade.php
+++ resources/views/vendor/adminlte/user-profile/index.blade.php
@@ -18,29 +18,38 @@
                     <table class="table table-hover">
                         <thead>
                         <thead>
+                        {{--<tr>--}}
+                            {{--<td>ID</td><td>:</td><td>{{Auth::user()->id}}</td>--}}
+                        {{--</tr>--}}
+
+                        {{--<tr>--}}
+                            {{--<td>ID profile</td><td>:</td><td>{{$profile->id}}</td>--}}
+                        {{--</tr>--}}
+
                         <tr>
-                            <td>ID</td><td>:</td><td>{{Auth::user()->id}}</td>
+                            <td>Name</td><td>:</td><td>{{$user->name}}</td>
                         </tr>
 
                         <tr>
-                            <td>ID profile</td><td>:</td><td>{{$profile->id}}</td>
+                            <td>Email</td><td>:</td><td>{{$user->email}}</td>
                         </tr>
 
                         <tr>
                             <td>Nama Lengkap</td><td>:</td><td>{{$profile->full_name}}</td>
                         </tr>
-                        <tr>
-                            <td>Email</td><td>:</td><td>{{$user->email}}</td>
-                        </tr>
+
                         <tr>
                             <td>Alamat</td><td>:</td><td>{{$profile->address}}</td>
                         </tr>
                         <tr>
                             <td>Tanggal Lahir</td><td>:</td><td>{{$profile->date_of_birth}}</td>
                         </tr>
+
                         <tr>
 
                             <td>Image Profile</td><td>:</td><td>{{$profile->profile_image}}</td>
+
+                            <a href="/images/{{$profile->profile_image}}" data-lightbox="roadtrip" a/>
                         </tr>
                         <tr>
                             <td>
