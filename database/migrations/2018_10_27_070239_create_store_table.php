23068cfd172d400012a22c10add20047607288dc
sjuliper7 <sjuliper7@gmail.com>
2018-11-15 22:14:33 +0700

    change name attribute

--- database/migrations/2018_10_27_070239_create_store_table.php
+++ database/migrations/2018_10_27_070239_create_store_table.php
@@ -29,7 +29,7 @@ class CreateStoreTable extends Migration
             $table->string('store_account_bank_image');
             $table->string('store_province');
             $table->string('store_districts');
-            $table->string('store_sub_distric');
+            $table->string('store_sub_district');
             $table->integer('id_user')->unsigned();
             $table->integer('id_request')->unsigned();
             $table->timestamps();
