0bca68f276184a9c96e6593f6c225de73c2a4a81
sjuliper7 <sjuliper7@gmail.com>
2018-11-15 23:50:20 +0700

    modifying prove-payment to prove_payment

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\DetailTransaction;
+use App\StatusTransaction;
 use App\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,14 +19,16 @@ class TransactionController extends Controller
     public function confirmPayment(Request $request)
     {
         $carts = Auth::user()->carts;
-        dd($carts);
+        $statusTransactions = StatusTransaction::where(['name'=> "Menunggu Pembayaran"])->firstOrFail();
+
         $transaction = new Transaction();
         $transaction->total_price =$request["total"];
         $transaction->shipment_fee = $request["shipment_fee"];
         $transaction->shipment_etd = $request["shipment_etd"];
+        $transaction->prove_payment = "-";
         $transaction->address = $request["address"];
         $transaction->id_user = Auth::user()->id;
-        $transaction->id_status = 1;
+        $transaction->id_status = $statusTransactions->id;
         $transaction->save();
 
         foreach ($carts as $cart){
@@ -36,9 +39,9 @@ class TransactionController extends Controller
             $detailTransaksi->id_transaction = $transaction->id;
             $detailTransaksi->id_product = $cart->id_product;
             $detailTransaksi->id_cart = $cart->id;
-//            $detailTransaksi->save();
+            $detailTransaksi->save();
             $cart->is_active = false;
-//            $cart->save();
+            $cart->save();
         }
         
         dd($request["address"], $request["total"], $request["shipment_fee"], $request["shipment_etd"]);
