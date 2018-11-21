3cd56072b026d4f47f16abce57c18d5fd3a4bfbd
sjuliper7 <sjuliper7@gmail.com>
2018-11-21 16:48:28 +0700

    remove LaravelDusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Dusk\DuskServiceProvider;
+//use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
+//            $this->app->register(DuskServiceProvider::class);
         }
     }
 }
