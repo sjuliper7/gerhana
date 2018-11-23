2a988ca08281ba4d66d665516ca3fff5691bfa59
sjuliper7 <sjuliper7@gmail.com>
2018-11-23 14:56:15 +0700

    create migration for review table

---
+++ database/migrations/2018_11_22_112050_crate_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('comment');
+            $table->integer('rating');
+            $table->boolean('approved');
+            $table->boolean('is_spam');
+            $table->timestamps();
+            $table->integer('id_product');
+            $table->integer('id_user');
+
+            $table->foreign('id_product')->references('id')->on('products');
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
