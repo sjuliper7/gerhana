f1e210dc5e20f92a16a7f246760c60a162021a24
sjuliper7 <sjuliper7@gmail.com>
2018-11-26 13:39:10 +0700

    modifying page structure for how to showing data

--- resources/views/vendor/adminlte/catalog-products/index.blade.php
+++ resources/views/vendor/adminlte/catalog-products/index.blade.php
@@ -4,79 +4,86 @@
 
 @section('main-content')
     <div class="container">
-        <div class="row">
+        <div class="row col-md-offset-0">
             <ul class="nav nav-tabs">
-                <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-                <li><a data-toggle="tab" href="#menu1">Properti</a></li>
-                <li><a data-toggle="tab" href="#menu2">Buku</a></li>
-                <li><a data-toggle="tab" href="#menu3">Ukiran</a></li>
-                <li><a data-toggle="tab" href="#menu4">Cenderamata</a></li>
-                <li><a data-toggle="tab" href="#menu5">Pakaian</a></li>
+                @foreach($categories as $category)
+                    @if($category->id == 1)
+                        <li class="active"><a data-toggle="tab" href="{{'#menu'.$category->id}}">{{$category->name}}</a></li>
+                    @else
+                        <li><a data-toggle="tab" href="{{'#menu'.$category->id}}">{{$category->name}}</a></li>
+                    @endif
+                @endforeach
             </ul>
-
             <div class="tab-content">
-                <div id="home" class="tab-pane fade in active">
-                    <h3>HOME</h3>
-                    <p>Some content.</p>
-                </div>
-                <div id="menu1" class="tab-pane fade">
-                    <h3>Properti</h3>
-                    <p>Some content in menu 1.</p>
-                </div>
-                <div id="menu2" class="tab-pane fade">
-                    <h3>Buku</h3>
-                    <p>Some content in menu 2.</p>
-                </div>
-                <div id="menu3" class="tab-pane fade">
-                    <h3>Ukiran</h3>
-                    <p>Some content in menu 3.</p>
-                </div>
-                <div id="menu4" class="tab-pane fade">
-                    <h3>Cenderamata</h3>
-                    <p>Some content in menu 4.</p>
-                </div>
-                <div id="menu5" class="tab-pane fade">
-                    <h3>Pakaian</h3>
-                    <p>Some content in menu 5.</p>
-                </div>
+                @foreach($categories as $category)
+                    @if($category->id == 1)
+                        <div id="{{'menu'.$category->id}}" class="tab-pane fade in active">
+                            <div class="col-md-10 col-md-offset-0" style="margin-top: 20px">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading"><h5>{{$category->name}}</h5></div>
+                                    <table class="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>Store Name</th>
+                                            <th>Product Name</th>
+                                            <th>Product Image</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($products as $product)
+                                            @if($product->id_category == $category->id)
+                                                <tr>
+                                                    <td>{{$product->store->store_name}}</td>
+                                                    <td>{{$product->name}}</td>
+                                                    <td>{{$product->image}}</td>
+                                                </tr>
+                                            @endif
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    @else
+                        <div id="{{'menu'.$category->id}}" class="tab-pane fade">
+                            <div class="col-md-10 col-md-offset-0" style="margin-top: 20px">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading"><h5>{{$category->name}}</h5></div>
+                                    <table class="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>Product Image</th>
+                                            <th>Store Name</th>
+                                            <th>Product Name</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($products as $product)
+                                            @if($product->id_category == $category->id)
+                                                <?php
+                                                    $images = json_decode($product->images);
+                                                ?>
+                                                <tr>
+                                                    <td><img src="{{asset('images/'.$images[0])}}" style="max-width: 100px;max-height: 100px; padding: 5px"></td>
+                                                    <td>{{$product->store->store_name}}</td>
+                                                    <td>{{$product->name}}</td>
+                                                </tr>
+                                            @endif
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
             </div>
 
             {{--<div class="col-md-4">--}}
                 {{--coba--}}
             {{--</div>--}}
 
-            {{--<div class="col-md-9 col-md-offset-1">--}}
-                {{--<div class="panel panel-default">--}}
-                    {{--<div class="panel-heading"><h3>Katalog Properti</h3></div>--}}
 
-                    {{--<table class="table table-hover">--}}
-                        {{--<thead>--}}
-                        {{--<tr>--}}
-                            {{--<th>Product Name</th>--}}
-                            {{--<th>Product Image</th>--}}
-                            {{--<th colspan="2">Actions</th>--}}
-                        {{--</tr>--}}
-                        {{--</thead>--}}
-                        {{--<tbody>--}}
-                        {{--<tr>--}}
-                            {{--<td></td>--}}
-                            {{--<td></td>--}}
-                            {{--<td>--}}
-                                {{--<a class="btn btn-small btn-info" href="#">Edit</a>--}}
-                            {{--</td>--}}
-                            {{--<td>--}}
-                                {{--<form action="#" method="post">--}}
-                                    {{--{{csrf_field()}}--}}
-                                    {{--<input name="_method" type="hidden" value="DELETE">--}}
-                                    {{--<button class="btn btn-danger" onclick="return confirm('Are you sure?')" type="submit">Delete</button>--}}
-                                {{--</form>--}}
-                            {{--</td>--}}
-                        {{--</tr>--}}
-
-                        {{--</tbody>--}}
-                    {{--</table>--}}
-                {{--</div>--}}
-            {{--</div>--}}
         </div>
     </div>
 @endsection
