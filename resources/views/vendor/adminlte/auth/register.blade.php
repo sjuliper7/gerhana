19ff4b53c8ddcb8578a95b099d1291f8019faa34
Johan Radot Mangaratua Lumban Batu <johanlbatu@gmail.com>
2018-11-27 20:37:59 +0700

    responsive register layout

--- resources/views/vendor/adminlte/auth/register.blade.php
+++ resources/views/vendor/adminlte/auth/register.blade.php
@@ -2,14 +2,17 @@
 <html lang="en">
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="{!! asset('template/styles/bootstrap4/bootstrap.min.css') !!}">
     <link rel="stylesheet" type="text/css"
           href="{!! asset('template/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css') !!}">
     <link rel="stylesheet" type="text/css" href="{!! asset('template/styles/bootstrap4/login.css') !!}">
-    <div class="text-center" style="margin-top: 0em">
-        <a href="{{ url('/home') }}">
-            <img src="{{asset('images/tittle.png')}}" alt="Responsive image" style="width: 15%;height: 15%">
-        </a>
+    <div class=".col-lg-12 .col-md-12 .col-sm-12">
+        <div class="text-center" style="margin-top: 0em">
+            <a href="{{ url('/home') }}">
+                <img src="{{asset('images/tittle.png')}}" alt="Responsive image" style="max-width: 100%;max-height: 100%">
+            </a>
+        </div>
     </div>
 
 </head>
@@ -17,103 +20,117 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-md-6">
-            <div class="text-center" style="margin-top: 5em">
-                <a href="{{ url('/home') }}">
-                    <img src="images/register.png" alt="" style="width: 100%;height: 100%">
-                </a>
-            </div>
+
+        <div class="col-lg-6 .col-md-4 .col-sm-6">
             <div class="text-center">
+                <a href="{{ url('/home') }}">
 
-            <b><h3> Jual beli mudah hanya di BatakZone</h3></b>
-            <b><h5 class="font-weight-normal">Gabung dan rasakan kemudahan bertransaksi di BatakZone</h5></b>
+                    <img class="image wrapper" src="images/register.png" alt="" style="width: 100%;height: auto">
+
+                </a>
+
+                <b><h3> Jual beli mudah hanya di BatakZone</h3></b>
+                <b><h5 class="font-weight-normal">Gabung dan rasakan kemudahan bertransaksi di BatakZone</h5></b>
+            </div>
+
         </div>
-    </div>
 
-    <div class="col-md-6">
-        <div class="row">
-            <div class="col-sm-9 " style="margin-left: 5em">
-                <div class="card card-signin my-5">
-                    <div class="card-body">
+        <div class="" style="margin-left: 5em">
 
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+        </div>
 
-                        <b><h5 class="card-title text-center">Daftar Sekarang</h5></b>
-                        <p class="text-center font-weight-normal">Sudah punya akun BatakZone? <a
-                                href="{{ url('/login') }}"> Masuk</a></p><br>
+        <div class=".col-lg-6 .col-md-4">
+            <div class="row">
+                <div class=".col-lg-12 .col-md-6">
+                    <div class="card card-signin my-10">
+                        <div class="card-body">
 
-                        <form action="{{ url('/register') }}" method="post" class="form-signin form-group">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <div class="form-label-group">
-                                <input type="text" name="name" id="inputNama" style="width: 21em" class="rounded-0"
-                                       placeholder="Nama"
-                                       required autofocus>
-                                <label for="inputNama">Nama</label>
-                            </div>
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
 
-                            <div class="form-label-group">
-                                <input type="email" name="email" id="inputEmail" style="width: 21em" class="rounded-0"
-                                       placeholder="Email address"
-                                       required autofocus>
-                                <label for="inputEmail">Email address</label>
-                            </div>
+                            <b><h5 class="card-title text-center">Daftar Sekarang</h5></b>
+                            <p class="text-center font-weight-normal">Sudah punya akun BatakZone? <a
+                                    href="{{ url('/login') }}"> Masuk</a></p><br>
 
-                            <div class="form-label-group">
-                                <input type="password" name="password" id="inputPassword" style="width: 21em" class="rounded-0"
-                                       placeholder="Password" required>
-                                <label for="inputPassword">Password</label>
-                            </div>
+                            <form action="{{ url('/register') }}" method="post" class="form-signin form-group">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <div class="form-label-group">
+                                    <input type="text" name="name" id="inputNama" style="width: 21em" class="rounded-0"
+                                           placeholder="Nama"
+                                           required autofocus>
+                                    <label for="inputNama">Nama</label>
+                                </div>
 
-                            <div class="form-label-group">
-                                <input type="password" name="password_confirmation" id="inputRePassword" style="width: 21em" class="rounded-0"
-                                       placeholder="Retype-Password" required>
-                                <label for="inputRePassword">Retype-Password</label>
-                            </div>
-                            <div class="form-label-group">
-                                <div class="col-xs-1">
-                                    <label>
-                                        <div class="checkbox_register icheck">
+                                <div class="form-label-group">
+                                    <input type="email" name="email" id="inputEmail" style="width: 21em"
+                                           class="rounded-0"
+                                           placeholder="Email address"
+                                           required autofocus>
+                                    <label for="inputEmail">Email address</label>
+                                </div>
+
+                                <div class="form-label-group">
+                                    <input type="password" name="password" id="inputPassword" style="width: 21em"
+                                           class="rounded-0"
+                                           placeholder="Password" required>
+                                    <label for="inputPassword">Password</label>
+                                </div>
+
+                                <div class="form-label-group">
+                                    <input type="password" name="password_confirmation" id="inputRePassword"
+                                           style="width: 21em" class="rounded-0"
+                                           placeholder="Retype-Password" required>
+                                    <label for="inputRePassword">Retype-Password</label>
+                                </div>
+                                <div class="form-label-group">
+                                    <div class="col-xs-1">
+                                        <label>
+                                            <div class="checkbox_register icheck">
 
                                                 <input type="checkbox" name="terms">
-                                                <button type="button" class=" text-center btn btn-outline-light text-dark" data-toggle="modal" data-target="#termsModal">{{ trans('adminlte_lang::message.terms') }}</button>
+                                                <button type="button"
+                                                        class=" text-center btn btn-outline-light text-dark"
+                                                        data-toggle="modal"
+                                                        data-target="#termsModal">{{ trans('adminlte_lang::message.terms') }}</button>
 
 
-                                        </div>
+                                            </div>
 
 
-                                    </label>
-                                </div><!-- /.col -->
-                            </div>
+                                        </label>
+                                    </div><!-- /.col -->
+                                </div>
 
-                            <button class="btn btn-lg btn-primary btn-block text-uppercase rounded" type="submit">Daftar
-                                Sekarang
-                            </button>
+                                <button class="btn btn-lg btn-primary btn-block text-uppercase rounded" type="submit">
+                                    Daftar
+                                    Sekarang
+                                </button>
 
-                            <hr class="my-4">
+                                <hr class="my-4">
 
-                            <button class="btn btn-lg btn-google btn-block text-uppercase rounded" type="submit"
-                                    style="background: #ea4335;"><i class="fab fa-google mr-2"></i> Sign up with
-                                Google
-                            </button>
-                            <button class="btn btn-lg btn-facebook btn-block text-uppercase rounded" type="submit"><i
-                                    class="fab fa-facebook-f mr-2"></i> Sign up with Facebook
-                            </button>
-                        </form>
+                                <button class="btn btn-lg btn-google btn-block text-uppercase rounded" type="submit"
+                                        style="background: #ea4335;"><i class="fab fa-google mr-2"></i> Sign up with
+                                    Google
+                                </button>
+                                <button class="btn btn-lg btn-facebook btn-block text-uppercase rounded" type="submit">
+                                    <i
+                                        class="fab fa-facebook-f mr-2"></i> Sign up with Facebook
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 </div>
 
@@ -122,5 +139,3 @@
 
 </body>
 </html>
-
-
