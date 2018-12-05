7e4d33edd775d4aa0ad0f1374deb322c8851dcc5
sjuliper7 <sjuliper7@gmail.com>
2018-12-05 18:14:00 +0700

    add table in container detail transaction

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -14,27 +14,48 @@
                                 <div class="cart_items">
                                     <ul class="cart_list">
                                         <li class="cart_item clearfix">
-                                            <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between col-lg-12">
-                                                <div class="cart_item_name cart_info_col col-lg-3">
-                                                    <div class="cart_item_title">Total Harga</div>
-                                                    <div>Rp {{number_format($transaction->total_price)}}</div>
-                                                </div>
-                                                <div class="cart_item_price cart_info_col col-lg-3">
-                                                    <div class="cart_item_title">Biaya Pengiriman</div>
-                                                    <div>Rp {{number_format($transaction->shipment_fee)}}</div>
-                                                </div>
-                                                <div class="cart_item_price cart_info_col col-lg-3">
-                                                    <div class="cart_item_title">Estimasi Pengiriman</div>
-                                                    <div>{{$transaction->shipment_etd}} Hari</div>
-                                                </div>
-                                                <div class="cart_item_price cart_info_col col-lg-2">
-                                                    <div class="cart_item_title">Total Barang</div>
-                                                    <div>{{count($transaction->detailTransactions)}}</div>
-                                                </div>
-                                                <div class="cart_item_price cart_info_col col-lg-3">
-                                                    <div class="cart_item_title">Status Barang</div>
-                                                    <div class=""><p class="text-danger">{{$transaction->status->name}}</p></div>
-                                                </div>
+                                            <div class="container">
+                                                <table class="table" style="margin-bottom: -10px;margin-top: -10px">
+                                                    <tr>
+                                                        <th style="border-top: none;" width="15%">Nomor Order</th>
+                                                        <th style="border-top: none;" width="20%">Total Price</th>
+                                                        <th style="border-top: none;" width="18%">Biaya Pengiriman</th>
+                                                        <th style="border-top: none;" width="20%">Estimasi Pengiriman</th>
+                                                        <th style="border-top: none;" width="15%">Total Barang</th>
+                                                        <th style="border-top: none;" width="25%">Status</th>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>#{{$transaction->order_id}}</td>
+                                                        <td>Rp {{number_format($transaction->total_price)}}</td>
+                                                        <td>Rp {{number_format($transaction->shipment_fee)}}</td>
+                                                        <td>{{$transaction->shipment_etd}} Hari</td>
+                                                        <td>{{count($transaction->detailTransactions)}}</td>
+                                                        <td class="text-danger">{{$transaction->status->name}}</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+
+                                            {{--<div class="cart_item_info d-flex flex-md-row flex-column justify-content-between col-lg-12">--}}
+                                                {{--<div class="cart_item_name cart_info_col col-lg-3">--}}
+                                                    {{--<div class="cart_item_title">Total Harga</div>--}}
+                                                    {{--<div>Rp {{number_format($transaction->total_price)}}</div>--}}
+                                                {{--</div>--}}
+                                                {{--<div class="cart_item_price cart_info_col col-lg-3">--}}
+                                                    {{--<div class="cart_item_title">Biaya Pengiriman</div>--}}
+                                                    {{--<div>Rp {{number_format($transaction->shipment_fee)}}</div>--}}
+                                                {{--</div>--}}
+                                                {{--<div class="cart_item_price cart_info_col col-lg-3">--}}
+                                                    {{--<div class="cart_item_title">Estimasi Pengiriman</div>--}}
+                                                    {{--<div>{{$transaction->shipment_etd}} Hari</div>--}}
+                                                {{--</div>--}}
+                                                {{--<div class="cart_item_price cart_info_col col-lg-2">--}}
+                                                    {{--<div class="cart_item_title">Total Barang</div>--}}
+                                                    {{--<div>{{count($transaction->detailTransactions)}}</div>--}}
+                                                {{--</div>--}}
+                                                {{--<div class="cart_item_price cart_info_col col-lg-3">--}}
+                                                    {{--<div class="cart_item_title">Status Barang</div>--}}
+                                                    {{--<div class=""> <p class="text-danger">{{$transaction->status->name}}</p></div>--}}
+                                                {{--</div>--}}
                                             </div>
                                         </li>
                                     </ul>
