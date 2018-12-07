ca8817ed70c92ddb57971dfa53630c0bd0649521
sjuliper7 <sjuliper7@gmail.com>
2018-12-07 14:53:36 +0700

    change color for text and structure layout

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -6,33 +6,32 @@
     <div class="cart_section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 offset-lg-0">
+                <div class="col-lg-12 col-md-offset-0">
                     <div class="cart_container">
-
                         @foreach($transactions as $transaction)
-                            <a href="{{url('transactions/'.$transaction->id.'/show')}}">
+                            <a href="{{url('transactions/'.$transaction->id.'/show')}}" style="color: black">
                                 <div class="cart_items">
                                     <ul class="cart_list">
                                         <li class="cart_item clearfix">
                                             <div class="container">
                                                 <table class="table" style="margin-bottom: -10px;margin-top: -10px">
                                                     <tr>
-                                                        <th style="border-top: none;" width="15%"><center>Nomor Order</center></th>
-                                                        <th style="border-top: none;" width="15%"><center>Total Price</center></th>
-                                                        <th style="border-top: none;" width="18%"><center>Biaya Pengiriman</center></th>
-                                                        <th style="border-top: none;" width="20%"><center>Estimasi Pengiriman</center></th>
-                                                        <th style="border-top: none;" width="15%"><center>Total Barang</center></th>
-                                                        <th style="border-top: none;" width="25%"><center>Status</center></th>
-                                                        <th style="border-top: none;" width="25%"><center>Resi</center></th>
+                                                        <td class="text-center" style="border-top: none;" width="15%">Nomor Order</td>
+                                                        <td class="text-center" style="border-top: none;" width="15%">Total Price</td>
+                                                        <td class="text-center" style="border-top: none;" width="18%">Biaya Pengiriman</td>
+                                                        <td class="text-center" style="border-top: none;" width="20%">Estimasi Pengiriman</td>
+                                                        <td class="text-center" style="border-top: none;" width="15%">Total Barang</td>
+                                                        <td class="text-center" style="border-top: none;" width="25%">Status</td>
+                                                        <td class="text-center" style="border-top: none;" width="25%">Resi</td>
                                                     </tr>
                                                     <tr>
-                                                        <td>#{{$transaction->order_id}}</td>
-                                                        <td>Rp {{number_format($transaction->total_price)}}</td>
-                                                        <td>Rp {{number_format($transaction->shipment_fee)}}</td>
-                                                        <td>{{$transaction->shipment_etd}} Hari</td>
-                                                        <td>{{count($transaction->detailTransactions)}}</td>
-                                                        <td class="text-danger">{{$transaction->status->name}}</td>
-                                                        <td class="text-success">{{$transaction->shipment_number}}</td>
+                                                        <td class="text-center">#{{$transaction->order_id}}</td>
+                                                        <td class="text-center">Rp {{number_format($transaction->total_price)}}</td>
+                                                        <td class="text-center">Rp {{number_format($transaction->shipment_fee)}}</td>
+                                                        <td class="text-center">{{$transaction->shipment_etd}} Hari</td>
+                                                        <td class="text-center">{{count($transaction->detailTransactions)}}</td>
+                                                        <td class="text-danger text-center">{{$transaction->status->name}}</td>
+                                                        <td class="text-success text-center">{{$transaction->shipment_number}}</td>
                                                     </tr>
                                                 </table>
                                             </div>
@@ -41,6 +40,7 @@
                                 </div>
                             </a>
                         @endforeach
+                    </div>
                     </div>
                 </div>
             </div>
